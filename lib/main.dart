a6381fcfaab92df9841f5bfa0e7fcbdf73d3c607
souvik666 <peacedynasty0@gmail.com>
2020-09-02 21:51:17 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<void> fetchQuestions()async{
+    var
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz App"),
+        elevation: 0.0,
+      ),
+      body: Container(),
+    );
+  }
+}
